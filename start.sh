3e2e0a2e324822392cdb8df0f1526617f18bafee
Asjad Iftikhar <iftikharasjad@gmail.com>
2026-04-02 16:38:32 +0500

    correct port

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 alembic upgrade head
 
 echo "Starting server..."
-exec uvicorn main:app --host 0.0.0.0 --port 8080
+exec uvicorn main:app --host 0.0.0.0 --port 8000
